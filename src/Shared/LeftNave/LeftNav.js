0c6aa493bfe9087398eebb674c9a69a2c8c64eee
Majharul-Munna <majharulmunna21@gmail.com>
2022-10-26 00:53:58 +0600

    topics added

---
+++ src/Shared/LeftNave/LeftNav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LeftNav = () => {
+    const [topics, setTopics] = useState([]);
+
+    useEffect( () =>{
+        fetch('http://localhost:5000/courses')
+        .then(res => res.json())
+        .then(data => setTopics(data));
+
+    },[])
+
+    return (
+        <div>
+            <h2>Total Courses: {topics.length}</h2>
+            <div>
+                {
+                    topics.map(topic => <p key={topic.id}>
+                        <Link to={`/topic/detail/${topic.id}`}>{topic.name}</Link>
+                    </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default LeftNav;
